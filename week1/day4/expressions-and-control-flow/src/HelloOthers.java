976f5d38cbf29ecaaea80d909c20aa97d6cff01b
aronkaczur <kishanczur@gmail.com>
2020-05-21 14:34:04 +0200

    Updated GitIgnore and the first 3 project

---
+++ week1/day4/expressions-and-control-flow/src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello Zsombor!");
+        System.out.println("Hello  Sándor!");
+        System.out.println("Hello Szid!");
+    }
+}
