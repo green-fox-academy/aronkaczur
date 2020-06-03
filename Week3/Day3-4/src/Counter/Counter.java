6eeb703f23c39d5ece2155b0185c5c144c25f7de
aronkaczur <kishanczur@gmail.com>
2020-06-03 16:37:33 +0200

    Counter uploaded

---
+++ Week3/Day3-4/src/Counter/Counter.java
@@ -0,0 +1,37 @@
+package Counter;
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+
+public class Counter {
+    public int counterNumber;
+    public int number;
+    public int initialvalue;
+
+
+    public Counter(int counterNumber) {
+        this.counterNumber = counterNumber;
+        this.initialvalue = counterNumber;
+    }
+
+    public Counter(){
+        this.counterNumber = 0;
+        this.initialvalue = 0;
+
+    }
+    public void add(int number) {
+        this.counterNumber += number;
+    }
+    public void add(){
+        this.counterNumber++;
+    }
+    public int get() {
+        return this.counterNumber;
+    }
+
+    public void reset() {
+        this.counterNumber = this.initialvalue;
+    }
+
+
+}
